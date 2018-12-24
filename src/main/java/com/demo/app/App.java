a02a37f5710e0aa2b2666d75213874f8d2f47034
Jeff Sapia <jeffreysapia@gmail.com>
2018-12-23 20:07:54 -0800

    Initial Commit

---
+++ src/main/java/com/demo/app/App.java
@@ -0,0 +1,13 @@
+package com.demo.app;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class App {
+
+    public static void main(String[] args) {
+        log.info("Test");
+        log.error("Hello");
+    }
+
+}
