0fb7dc58af71d4e6833bfa629661bc7c36260a39
Unknown <jeffreysapia@gmail.com>
2018-12-28 10:38:49 -0800

    Update

--- src/main/java/com/demo/app/App.java
+++ src/main/java/com/demo/app/App.java
@@ -1,14 +1,16 @@
 package com.demo.app;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 public class App {
 
-    private static final double[] FILTER = {0.1, 0.2, 0.3};
+    private static final double[] FILTER = {0.1, 0.2, 0.3, 0.4, 0.5};
 
     public static void main(String[] args) {
         log.info("Begin test");
@@ -24,13 +26,6 @@ public class App {
             data.add(Math.random());
         }
         log.info(data.toString());
-    }
-
-    private int calculateFilterLookback(double[] filter) {
-        log.info("Calculating filter lookback length...");
-        int lookback = filter.length / 2;
-        log.info("Filter lookback: {}", lookback);
-        return lookback;
     }
 
     private void applyFilter(List<Double> data, double[] filter) {
@@ -50,16 +45,17 @@ public class App {
         // Validate input data
         validate(dataSize, filterSize);
 
-        List<Double> filteredOutput = Lists.newArrayList();
+        Map<String, Double> filteredArray = Maps.newLinkedHashMap();
         // Apply filter to data
         for (int i = startIndex; i < endIndex; i++) {
             List<Double> dataChunk = data.subList(i-lookback, i+(lookback+1));
             log.info("Data Chunk Index: {} to {}", i-lookback, i+lookback);
             log.info("Data Chunk: {}", dataChunk);
             double filteredValue = applyFilterSegment(dataChunk, filter);
-            filteredOutput.add(filteredValue);
-            log.info("Filtered Value: {}", filteredValue);
-            log.info("Filtered Output: {}", filteredOutput);
+            String label = String.format("y[%s]", i);
+            filteredArray.put(label, filteredValue);
+            log.info("Filtered Value y[{}]: {}", i, filteredValue);
+            log.info("Filtered Array: {}", filteredArray);
         }
     }
 
@@ -69,6 +65,13 @@ public class App {
             filteredValue += (data.get(i) * filter[i]);
         }
         return filteredValue;
+    }
+
+    private int calculateFilterLookback(double[] filter) {
+        log.info("Calculating filter lookback length...");
+        int lookback = filter.length / 2;
+        log.info("Filter lookback: {}", lookback);
+        return lookback;
     }
 
     private void validate(int dataSize, int filterSize) {
