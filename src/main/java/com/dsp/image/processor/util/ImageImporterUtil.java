33d0a9f88a5866bd8301bb4ead7e4fd9557546f8
Unknown <jeffreysapia@gmail.com>
2018-12-30 22:23:26 -0800

    Adding image exporter

--- src/main/java/com/dsp/image/processor/util/ImageImporterUtil.java
+++ src/main/java/com/dsp/image/processor/util/ImageImporterUtil.java
@@ -3,7 +3,9 @@ package com.dsp.image.processor.util;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -14,34 +16,62 @@ public class ImageImporterUtil {
         log.info("Importing image...");
         ImageImporterUtil imageImporterUtil = new ImageImporterUtil();
         try {
-            imageImporterUtil.getImageDimensions();
+            //imageImporterUtil.loadImageFromDisk();
+            imageImporterUtil.saveImageToDisk();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
 
-    private void getImageDimensions() throws IOException {
+    private void loadImageFromDisk() throws IOException {
         BufferedImage image = ImageIO.read(new File("test-01.png"));
-
         int width = image.getWidth();
         int height = image.getHeight();
-
         System.out.println("Width: " + width);
         System.out.println("Height: " + height);
-        for (int y=0; y < height; y++) {
+        for (int y = 0; y < height; y++) {
             StringBuilder sb = new StringBuilder();
-            for (int x=0; x < width; x++) {
-                int pixel = image.getRGB(x, y);
-                int alpha = (pixel >> 24) & 0xff;
-                int red = (pixel >> 16) & 0xff;
-                int green = (pixel >> 8) & 0xff;
-                int blue = (pixel) & 0xff;
-                String pixels = String.format("[%s %s %s]", red, green, blue);
-                sb.append(String.format("%-15s", pixels));
+            for (int x = 0; x < width; x++) {
+                Color color = new Color(image.getRGB(x, y), true);
+                String pixels = String.format("[%s %s %s]", color.getRed(), color.getGreen(), color.getBlue());
+                sb.append(String.format("%-14s", pixels));
             }
             System.out.println(sb.toString());
         }
+    }
+
+    private void saveImageToDisk() throws IOException {
+        BufferedImage image = ImageIO.read(new File("test-01.png"));
+        int width = image.getWidth();
+        int height = image.getHeight();
+        System.out.println("Width: " + width);
+        System.out.println("Height: " + height);
+        for (int y = 0; y < height; y++) {
+            StringBuilder sb = new StringBuilder();
+            for (int x = 0; x < width; x++) {
+                int pixel = image.getRGB(x, y);
+                Color color = new Color(pixel, true);
+
+
+                /**
+                 *
+                 * If pixel is black, convert pixel to blue
+                 *
+                 *
+                  */
+                if (color.getRed() == 0 && color.getGreen() == 0 && color.getBlue() == 0) {
+                    Color c2 = new Color(0, 0, 255);
+                    image.setRGB(x, y, c2.getRGB());
+                }
+
+
+                Color updatedColor = new Color(image.getRGB(x, y), true);
+                String pixels = String.format("[%s %s %s]",  updatedColor.getRed(), updatedColor.getGreen(), updatedColor.getBlue());
+                sb.append(String.format("%-14s", pixels));
+            }
+            System.out.println(sb.toString());
+        }
+        ImageIO.write(image, "png", new File("test-01-modded.png"));
     }
 
 }
